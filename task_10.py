51c8540329b9f4990e39aa8ef29fe3666d6ad335
sadyh <rusdyhov@mail.ru>
2022-05-28 01:11:47 +0300

    .

---
+++ task_10.py
@@ -0,0 +1,17 @@
+def count_words(string):
+    BOOK = 'qwertyuiopasdfghjklzxcvbnm '
+    st_arr = ''
+    dict1 = {}
+    for i in string.lower():
+        if i in BOOK:
+            st_arr += i
+    st_arr = st_arr.split(' ')
+    for i in st_arr:
+        if len(i) >= 1 and i != ' ':
+            if i not in dict1.keys():
+                dict1[i] = 1
+            else:
+                dict1[i] += 1
+    print(dict1)
+
+
