a53211da9f5c76da638e12627c3d200679c035a4
sadyh <rusdyhov@mail.ru>
2022-05-28 01:32:31 +0300

    .

---
+++ task_11.py
@@ -0,0 +1,27 @@
+class Desk:
+    def __set_name__(self, owner, name):
+        self.name = '_' + name
+
+    def __get__(self, instance, owner):
+        return getattr(instance, self.name)
+
+    def __set__(self, instance, value):
+        setattr(instance, self.name, value)
+
+
+class Dessert:
+    name = Desk()
+    calories = Desk()
+
+    def __init__(self, name='', calories=0):
+        self.name = name
+        self.calories = calories
+
+    def is_healthy(self):
+        if self.calories < 200:
+            return True
+        else:
+            return False
+
+    def is_delicious(self):
+        return True
